9fd198674938e88f9c8d40a147bc76d8be6430ff
elarhadu <elarhadu@gmail.com>
2023-03-09 17:28:45 +0000

    Create a many-to-many relationship between Person and Book class through rentals.

--- student.rb
+++ student.rb
@@ -12,6 +12,6 @@ class Student < Person
 
   def add_classroom(classroom)
     @classroom = classroom
-    classroom.students << self
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end

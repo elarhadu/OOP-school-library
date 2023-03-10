5353914779d12fbb20ecbac2269fe98f672534a7
elarhadu <elarhadu@gmail.com>
2023-03-10 11:43:59 +0000

    Modify create teacher

--- create_teacher.rb
+++ create_teacher.rb
@@ -1,12 +1,12 @@
 class CreateTeacher
-  def create_teacher(people)
-    @people = people
+  def create_teacher(person)
+    @person = person
     puts 'What is your name?'
     name = gets.chomp
     puts 'How old are you?'
     age = gets.chomp.to_i
     puts 'Enter your specialization'
     specialization = gets.chomp
-    @people.push(Teacher.new(name, age, specialization))
+    @person.push(Teacher.new(name, age, specialization))
   end
 end

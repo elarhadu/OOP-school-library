bb3b78babb1f2627cacf601304d0ff8da1c734a1
elarhadu <elarhadu@gmail.com>
2023-03-10 12:01:49 +0000

    Create book

---
+++ create_book.rb
@@ -0,0 +1,11 @@
+class CreateBook
+  def create_book(books)
+    @books=books
+    puts 'Enter book\'s title'
+    title=gets.chomp
+    puts 'Enter author\'s name'
+    author=gets.chomp
+    @books.push(Book.new(title,author))
+    puts 'Book has been successfully created'
+  end
+end

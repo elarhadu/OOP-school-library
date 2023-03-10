8f20c935d2638575b42f568ba268499e6ad6ef8d
elarhadu <elarhadu@gmail.com>
2023-03-10 11:37:02 +0000

    Create student

---
+++ create_student.rb
@@ -0,0 +1,25 @@
+class CreateStudent
+  def create_student(people)
+    @people = people
+    puts 'What is your name?'
+    name = gets.chomp
+    puts 'How old are you?'
+    age = gets.chomp.to_i
+    puts 'What grade are you?'
+    classroom = gets.chomp
+    parent_permission = parent_permission?
+    @people.push(Student.new(age, classroom, name, parent_permission: parent_permission))
+    puts 'Student Successfuly created'
+  end
+
+  def parent_permission?
+    puts 'Do you have parent permission? Y/N'
+    parent_permission = gets.chomp.upcase
+    case parent_permission
+    when 'Y' then true
+    when 'N' then false
+    else p 'Enter a valid value: Y or N'
+         parent_permission?
+    end
+  end
+end
